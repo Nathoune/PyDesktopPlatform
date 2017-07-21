d5b57b0a87fd819d5b3f4bef23f76ad439e8bcc3
Léo Dubus <dubusster@gmail.com>
2017-07-21 08:49:22 +0200

    pep8 cleaning

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from path import Path
 
 from physics.core import Position, Speed
 
-################################################ 
+################################################
 ################################################
 
 DATA_FOLDER = Path("data")
@@ -23,21 +23,21 @@ DATA_FOLDER = Path("data")
 ################################################
 ################################################
 
-WINDOWS_SIZE_X=1024
-WINDOWS_SIZE_Y=600
+WINDOWS_SIZE_X = 1024
+WINDOWS_SIZE_Y = 600
 
-PIKACHU_SIZE_X=100
-PIKACHU_SIZE_Y=100
+PIKACHU_SIZE_X = 100
+PIKACHU_SIZE_Y = 100
 
-INITIAL_X_POS=(WINDOWS_SIZE_X-PIKACHU_SIZE_X)/2
-INITIAL_Y_POS=(WINDOWS_SIZE_Y-PIKACHU_SIZE_Y)/2
+INITIAL_X_POS = (WINDOWS_SIZE_X - PIKACHU_SIZE_X) / 2
+INITIAL_Y_POS = (WINDOWS_SIZE_Y - PIKACHU_SIZE_Y) / 2
 
-MAX_X_SPEED=20
-MAX_Y_SPEED=20
+MAX_X_SPEED = 20
+MAX_Y_SPEED = 20
 
-BACK_COLOR=(0,0,0)  # 0,0,0 = Black
+BACK_COLOR = (0, 0, 0)  # 0,0,0 = Black
 
-FPS=30
+FPS = 30
 
 ################################################
 ################################################
@@ -49,23 +49,23 @@ FPS=30
 clock = pg.time.Clock()
 
 
-position=Position()
-speed=Speed()
+position = Position()
+speed = Speed()
 
-screen = pg.display.set_mode((WINDOWS_SIZE_X,WINDOWS_SIZE_Y))
-pikachu=pg.image.load(DATA_FOLDER / 'Pikachu-As-Ninja.png')
-pikachu=pg.transform.scale(pikachu, (PIKACHU_SIZE_X, PIKACHU_SIZE_Y))
+screen = pg.display.set_mode((WINDOWS_SIZE_X, WINDOWS_SIZE_Y))
+pikachu = pg.image.load(DATA_FOLDER / 'Pikachu-As-Ninja.png')
+pikachu = pg.transform.scale(pikachu, (PIKACHU_SIZE_X, PIKACHU_SIZE_Y))
 
-speed.init(0,0)
+speed.init(0, 0)
 position.init(INITIAL_X_POS, INITIAL_X_POS)
 
 # Refresh screen
-screen.blit(pikachu, (position.x,position.y))
+screen.blit(pikachu, (position.x, position.y))
 pg.display.flip()
 
 ################################################
 ################################################
-run=True
+run = True
 
 while run:
 
@@ -73,13 +73,17 @@ while run:
 
     pg.event.get()
 
-    keys = pg.key.get_pressed()             # Etat du clavier au moment t
-    if keys[K_RIGHT]: speed.edit(2,0)
-    if keys[K_LEFT]: speed.edit(-2,0)
-    if keys[K_UP]: speed.edit(0,-2)
-    if keys[K_DOWN]: speed.edit(0,2)
-    if keys[K_ESCAPE] : run=False
-
+    keys = pg.key.get_pressed()  # Etat du clavier au moment t
+    if keys[K_RIGHT]:
+        speed.edit(2, 0)
+    if keys[K_LEFT]:
+        speed.edit(-2, 0)
+    if keys[K_UP]:
+        speed.edit(0, -2)
+    if keys[K_DOWN]:
+        speed.edit(0, 2)
+    if keys[K_ESCAPE]:
+        run = False
 
     position.edit(speed,
                   WINDOWS_SIZE_X,
@@ -88,7 +92,7 @@ while run:
                   PIKACHU_SIZE_Y)
     # Clean sreen
     screen.fill(BACK_COLOR)
-    #Refresh screen
+    # Refresh screen
     screen.blit(pikachu, (position.x, position.y))
     pg.display.flip()
-    speed.init(0,0)
+    speed.init(0, 0)
--- physics/core.py
+++ physics/core.py
@@ -9,31 +9,32 @@ So far :
 @author: dubusster
 '''
 
+
 class Speed(object):
-    x=0
-    y=0
+    x = 0
+    y = 0
 
-    def init(self,x,y):
-        self.x=0
-        self.y=0
+    def init(self, x, y):
+        self.x = 0
+        self.y = 0
 
-    def edit(self,newX,newY):
-        self.x+=newX
-        self.y+=newY
+    def edit(self, newX, newY):
+        self.x += newX
+        self.y += newY
 
 #######################
 
+
 class Position(object):
-    y=0
-    x=0
-    
+    y = 0
+    x = 0
 
     def init(self, x, y):
-        self.y=y
-        self.x=x
+        self.y = y
+        self.x = x
 
     def edit(self, speed, win_x, win_y, obj_x, obj_y):
-        if self.x+speed.x<win_x-obj_x and self.x+speed.x>0 :
-            self.x+=speed.x
-        if self.y+speed.y<win_y-obj_y and self.y+speed.y>0 :
-            self.y+=speed.y
+        if self.x + speed.x < win_x - obj_x and self.x + speed.x > 0:
+            self.x += speed.x
+        if self.y + speed.y < win_y - obj_y and self.y + speed.y > 0:
+            self.y += speed.y
