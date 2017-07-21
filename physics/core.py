4cbba17001ca31874f61ec0c0224872b709264b4
Léo Dubus <dubusster@gmail.com>
2017-07-21 09:09:51 +0200

    added Entity class to create physical objects

--- physics/core.py
+++ physics/core.py
@@ -10,13 +10,32 @@ So far :
 '''
 
 
+class Entity(object):
+    '''
+    Super class that will be used to create objects in space
+
+    '''
+
+    def __init__(self, name, pos_x=0, pos_y=0, win_x=1024, win_y=600):
+        self.name = name
+        self.position = Position()
+        self.position.init(pos_x, pos_y)
+        self.speed = Speed()
+        self.win_size = (win_x, win_y)
+
+    def move(self):
+        win_x, win_y = self.win_size
+        self.position.edit(self.speed, win_x, win_y, self.x, self.y)
+
+
 class Speed(object):
-    x = 0
-    y = 0
+
+    def __init__(self, x=0, y=0):
+        self.init(x, y)
 
     def init(self, x, y):
-        self.x = 0
-        self.y = 0
+        self.x = x
+        self.y = y
 
     def edit(self, newX, newY):
         self.x += newX
@@ -26,12 +45,13 @@ class Speed(object):
 
 
 class Position(object):
-    y = 0
-    x = 0
+
+    def __init__(self, x=0, y=0):
+        self.init(x, y)
 
     def init(self, x, y):
-        self.y = y
         self.x = x
+        self.y = y
 
     def edit(self, speed, win_x, win_y, obj_x, obj_y):
         if self.x + speed.x < win_x - obj_x and self.x + speed.x > 0:
